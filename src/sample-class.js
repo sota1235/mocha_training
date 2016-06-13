7029cdc09d98942bddacb743e1de0d9172ce2618
sota1235 <sota1235@gmail.com>
2016-06-14 02:12:10 +0900

    modify: install babel and add sample file

---
+++ src/sample-class.js
@@ -0,0 +1,29 @@
+/**
+ * @description Sample class
+ */
+
+/**
+ * @class Dog
+ */
+class Dog {
+  /**
+   * @param {string} name
+   * @param {number} age
+   */
+  constructor(name) {
+    this.name = name;
+    this.age  = age;
+  }
+
+  callMyName() {
+    console.log(`My name is ${this.name}`);
+  }
+
+  static bark() {
+    console.log('Wow!');
+  }
+
+  get age() {
+    return this.age;
+  }
+}
