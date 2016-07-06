6957908f24a13cd0a8ab31f17a75af83369e0cc2
sota1235 <sota1235@gmail.com>
2016-07-06 14:05:23 +0900

    modify: add comments

--- tests/sample-test.js
+++ tests/sample-test.js
@@ -1,8 +1,11 @@
 import assert from 'assert';
 
+// describe() ... テストの仲間分け. 何回層でもネスト可能
 describe('Sample test', () => {
+  // it() ... ユニットテストの1単位
   it('1 is 1', done => {
     assert.equal(1, 1);
+    // done() ... これを実行することでテストの完了を知らせる
     done();
   });
 });
