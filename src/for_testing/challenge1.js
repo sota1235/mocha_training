7bea04f6ffcc4b234d2ec4b1c7aa289011e41784
sota1235 <sota1235@gmail.com>
2016-07-06 18:43:43 +0900

    modify: add method

--- src/for_testing/challenge1.js
+++ src/for_testing/challenge1.js
@@ -2,14 +2,28 @@
  * @description 四則計算を行うメソッド群
  */
 
+const isNumber = arg => typeof arg === 'number';
+
 /**
  * @description 渡された引数の足し算の結果を返す
  */
 export const add = (arg1, arg2) => {
   // 型チェック
-  if (!(typeof arg1 === 'number') || !(typeof arg2 === 'number')) {
+  if (!(isNumber(arg1)) || !(isNumber(arg2))) {
     throw new Error;
   }
 
   return arg1 + arg2;
+}
+
+/**
+ * @description 渡された引数の引き算の結果を返す
+ */
+export const minus = (arg1, arg2) => {
+  // 型チェック
+  if (!(isNumber(arg1)) || !(isNumber(arg2))) {
+    throw new Error;
+  }
+
+  return arg1 - arg2;
 }
