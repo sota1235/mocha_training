275e5f9767a39a14c1397df68776bd056f703db4
sota1235 <sota1235@gmail.com>
2016-07-07 02:44:51 +0900

    modify: fix files

---
+++ src/challenge1/calc.js
@@ -0,0 +1,31 @@
+/**
+ * @description 四則計算を行うメソッド群
+ */
+
+/**
+ * @description 渡された引数の足し算の結果を返す
+ */
+export const add = (arg1, arg2) => {
+  return arg1 + arg2;
+}
+
+/**
+ * @description 渡された引数の引き算の結果を返す
+ */
+export const minus = (arg1, arg2) => {
+  return arg1 - arg2;
+}
+
+/**
+ * @description 渡された引数の掛け算の結果を返す
+ */
+export const double = (arg1, arg2) => {
+  return arg1 * arg2;
+}
+
+/**
+ * @description 渡された引数の割り算の結果を返す
+ */
+export const devide = (arg1, arg2) => {
+  return arg1 / arg2;
+}
