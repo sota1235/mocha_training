135b61841cdb3e22084c31596714d3a2a983953d
sota1235 <sota1235@gmail.com>
2016-07-06 18:55:12 +0900

    modify: add methoda and refactor

--- src/for_testing/challenge1.js
+++ src/for_testing/challenge1.js
@@ -2,17 +2,31 @@
  * @description 四則計算を行うメソッド群
  */
 
+/**
+ * @description 型が数値型かどうかチェック
+ * @param {mixed} arg
+ * @return {boolean}
+ */
 const isNumber = arg => typeof arg === 'number';
+
+/**
+ * @description 計算前の型チェック処理
+ * @param {mixed} arg1
+ * @param {mixed} arg2
+ * @return {void}
+ * @throws {Error}
+ */
+const checkArgs = (arg1, arg2) => {
+  if (!(isNumber(arg1)) || !(isNumber(arg2))) {
+    throw new Error;
+  }
+};
 
 /**
  * @description 渡された引数の足し算の結果を返す
  */
 export const add = (arg1, arg2) => {
-  // 型チェック
-  if (!(isNumber(arg1)) || !(isNumber(arg2))) {
-    throw new Error;
-  }
-
+  checkArgs(arg1, arg2);
   return arg1 + arg2;
 }
 
@@ -20,10 +34,14 @@ export const add = (arg1, arg2) => {
  * @description 渡された引数の引き算の結果を返す
  */
 export const minus = (arg1, arg2) => {
-  // 型チェック
-  if (!(isNumber(arg1)) || !(isNumber(arg2))) {
-    throw new Error;
-  }
-
+  checkArgs(arg1, arg2);
   return arg1 - arg2;
+}
+
+/**
+ * @description 渡された引数の掛け算の結果を返す
+ */
+export const double = (arg1, arg2) => {
+  checkArgs(arg1, arg2);
+  return arg1 * arg2;
 }
