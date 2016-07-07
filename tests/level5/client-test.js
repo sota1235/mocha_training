deabb7987d379844d8a10de7462878d5059b892a
sota1235 <sota1235@gmail.com>
2016-07-07 13:37:17 +0900

    add: sample

---
+++ tests/level5/client-test.js
@@ -0,0 +1,13 @@
+/**
+ * @description src/level5/client.jsのユニットテスト
+ */
+
+import assert     from 'assert';
+import proxyquire from 'proxyquire';
+import { getUsers } from '../../src/level5/client';
+
+describe('Unit tests for src/level5/client.js', () => {
+  it('通信が成功したとき', () => {
+    // TODO
+  });
+});
