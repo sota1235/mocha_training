597d6a1ee0a0097adfdde23af8128fb6afa2cb0f
sota1235 <sota1235@gmail.com>
2016-07-07 12:07:13 +0900

    add: level3

---
+++ answers/level3/is-even-test.js
@@ -0,0 +1,10 @@
+/**
+ * @description src/level3/is-even.jsのユニットテスト
+ */
+
+import assert from 'assert';
+import { isEven } from '../../src/level3/is-even';
+
+describe('Unit tests for is-even', () => {
+  // TODO: TDD!!
+});
---
+++ src/level3/is-even.js
@@ -0,0 +1,7 @@
+/**
+ * @description 偶数かどうかチェックする関数
+ */
+
+export const isEven = number => {
+  // TODO: Make me!!!
+}
