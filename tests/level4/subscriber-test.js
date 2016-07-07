9f8ab5b3344f0d602eeba1c2e37cf6b7ed7845c7
sota1235 <sota1235@gmail.com>
2016-07-07 12:24:15 +0900

    modify: add template for level4

---
+++ tests/level4/subscriber-test.js
@@ -0,0 +1,14 @@
+/**
+ * @description src/level4/subscriber.jsのユニットテスト
+ */
+
+import assert from 'assert';
+import sinon  from 'sinon';
+import emitter from '../../src/level4/event-emitter';
+import { registEventListener } from '../../src/level4/subscriber';
+
+describe('Unit tests for src/level4/subscriber', () => {
+  it('登録したイベントリスナがきちんと実行されることをテスト', () => {
+    // TODO
+  });
+});
