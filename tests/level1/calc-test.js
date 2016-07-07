ddb64e73eb971611d14c4a297c29f4d0bc2bab14
sota1235 <sota1235@gmail.com>
2016-07-07 12:10:36 +0900

    modify: add sample tests

---
+++ tests/level1/calc-test.js
@@ -0,0 +1,40 @@
+/**
+ * @description Unit tests for src/level1/calc.js
+ */
+
+import assert from 'assert';
+import { add, minus, double, devide } from '../../src/level1/calc-improved';
+
+describe('challenge1.js Test', () => {
+  describe('add() method test', () => {
+    /** 正常テスト */
+    it('1 + 1 = 2', () => {
+      // TODO
+    });
+
+    it('1 + (-1) = 0', () => {
+      // TODO
+    });
+
+    it('2番目以降の引数を受け付けないことをチェック', () => {
+      // TODO
+    });
+
+    /** 異常系テスト */
+    it('文字は受け付けないことをチェック', () => {
+      // TODO
+    });
+  });
+
+  describe('minus() method test', () => {
+    // TODO
+  });
+
+  describe('double() method test', () => {
+    // TODO
+  });
+
+  describe('devide() method test', () => {
+    // TODO
+  });
+});
---
+++ tests/level2/string-util-test.js
@@ -0,0 +1,22 @@
+/**
+ * @description Unit tests for src/level2/string-util
+ */
+
+import assert from 'assert';
+import { escape, isValidUrl, getClassName } from '../../src/level2/string-util';
+
+describe('Unit tests for string-util', () => {
+  describe('escape() test', () => {
+    it('HTML文字列をエスケープすること', () => {
+      // TODO
+    });
+  });
+
+  describe('isValidUrl() test', () => {
+    // TODO
+  });
+
+  describe('getClassName() test', () => {
+    // TODO
+  });
+});
